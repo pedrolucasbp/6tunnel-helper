f458f1c584d0058596e5c08af6b71ad9c29767a3
patrick-birkle <internet@birkle.email>
2021-01-31 13:05:27 +0100

    fixed permissions

--- setup.sh
+++ setup.sh
@@ -14,38 +14,42 @@ function check_preconditions() {
 
 function prepare_environment_variables() {
   readonly SCRIPT_DIR="${0%/*}"
-  readonly 6TUNNEL_CONFIG_DIR=/etc/6tunnel
-  readonly 6TUNNEL_SERVICE_DIR="${6TUNNEL_CONFIG_DIR}"/service
+  readonly CONFIG_DIR="/etc/6tunnel"
+  readonly SERVICE_DIR="${CONFIG_DIR}/service"
 }
 
 function setup_service() {
   # add config and service scripts
-  mkdir -p "${6TUNNEL_SERVICE_DIR}"
-  if ! [[ -e ${6TUNNEL_CONFIG_DIR}/config ]]; then
-    cp "${SCRIPT_DIR}"/config/config "${6TUNNEL_CONFIG_DIR}"
+  mkdir -p "${SERVICE_DIR}"
+  if ! [[ -e ${CONFIG_DIR}/config ]]; then
+    cp "${SCRIPT_DIR}"/config/config "${CONFIG_DIR}"
   fi
-  yes | cp "${SCRIPT_DIR}"/service/start.sh "${6TUNNEL_SERVICE_DIR}"
-  yes | cp "${SCRIPT_DIR}"/service/stop.sh "${6TUNNEL_SERVICE_DIR}"
+  yes | cp "${SCRIPT_DIR}"/service/start.sh "${SERVICE_DIR}"
+  yes | cp "${SCRIPT_DIR}"/service/stop.sh "${SERVICE_DIR}"
+  chown -R root:root "${SERVICE_DIR}"
+  chmod -R 744 "${SERVICE_DIR}"
 
   # add service file
   cp "${SCRIPT_DIR}"/service/6tunnel.service /etc/systemd/system/
 
-  systemctl enable 6tunnel
+  systemctl enable 6tunnel | exit 1
 }
 
 function setup_finished() {
   echo "Setup of 6tunnel service finished successfully"
   echo ""
   echo "  1. edit config for your bindings in /etc/6tunnel/config"
-  echo "     vi /etc/6tunnel.config"
+  echo "     sudo vi /etc/6tunnel/config"
   echo ""
   echo "  2. start 6tunnel service"
-  echo "     systemctl start 6tunnel"
+  echo "     sudo systemctl start 6tunnel"
   echo ""
   echo "  3. check if bindings exist"
   echo "     ps -aux | grep 6tunnel"
+  echo ""
 }
 
 check_preconditions
 prepare_environment_variables
 setup_service
+setup_finished
