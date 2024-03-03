4a2fd67a52d1562ce43eefca7380ee50cf67441b
Pedro Lucas <pedrolucasbp@gmail.com>
2024-03-03 01:27:21 +0100

    Changes from config directory to conf.d and names from 6tunnel to 6tunnel-helper.

--- service/start.sh
+++ service/start.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/env bash
+
 
 readonly regex="^[0-9]+,[a-zA-Z0-9.-]+,[0-9]+$"
 
-grep -E "${regex}" < /etc/6tunnel/config | while IFS= read -r config
+grep -E "${regex}" "/etc/6tunnel/config.d/*.conf" | while IFS= read -r config
 do
   LOCAL_PORT=$(echo "${config}" | awk -F',' '{print $1}')
   TARGET_HOST=$(echo "${config}" | awk -F',' '{print $2}')
