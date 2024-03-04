fe02dd0d23994b5550b0dd4c27e2ec1be78d0a20
Pedro Lucas <pedrolucasbp@gmail.com>
2024-03-04 23:51:53 +0100

    Changes on start script to reflect the changes on config files.

--- service/6tunnel-helper-start.sh
+++ service/6tunnel-helper-start.sh
@@ -1,12 +1,19 @@
 #!/bin/env bash
 
 
-readonly regex="^[0-9]+,[a-zA-Z0-9.-]+,[0-9]+$"
+readonly regex="^[0-9]+,.*,[0-9]+$"
 
-grep -E "${regex}" "/etc/6tunnel/config.d/*.conf" | while IFS= read -r config
-do
-  LOCAL_PORT=$(echo "${config}" | awk -F',' '{print $1}')
-  TARGET_HOST=$(echo "${config}" | awk -F',' '{print $2}')
-  TARGET_PORT=$(echo "${config}" | awk -F',' '{print $3}')
-  6tunnel "${LOCAL_PORT}" "${TARGET_HOST}" "${TARGET_PORT}"
+for conf_file in $(find /etc/6tunnel-helper/conf.d/ -type f -name "*.conf") 
+
+  OPTIONS="$(grep "OPTIONS" "${conf_file}" | cut -d "=" -f 2 | tr -d \")"
+  
+  for config in $(grep -E "^[0-9]+.*\,[0-9]+$" ${conf_file}); do
+
+    LOCAL_PORT=$(echo "${config}" | awk -F',' '{print $1}')
+    TARGET_HOST=$(echo "${config}" | awk -F',' '{print $2}')
+    TARGET_PORT=$(echo "${config}" | awk -F',' '{print $3}')
+    
+    6tunnel "${OPTIONS}" "${LOCAL_PORT}" "${TARGET_HOST}" "${TARGET_PORT}"
+  done
+
 done
